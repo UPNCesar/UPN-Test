b2cf94bf830217ed5499a06c7948df17231fc93e
UPNCesar <n00456648@upn.pe>
2025-06-01 21:15:38 -0500

    Archivo de instrucciones agregadas

--- app/fomularios/asisteas.cs
+++ app/fomularios/asisteas.cs
@@ -12,13 +12,14 @@ namespace app.fomularios
 {
     public partial class asisteas : Form
     {
-        public asisteas(string app_name, string app_version, int codigo)
+        public asisteas(string app_name, string app_version)
         {
             InitializeComponent();
 
             //el 100 a 999 padres, 1000 hasta 9999 profesores, 10000 hasta 100000 directores
 
-            string hola = Txt1.Text;
+            //string hola = Txt1.Text;
+
         }
 
         private void Boton_asistencias_Click(object sender, EventArgs e)
